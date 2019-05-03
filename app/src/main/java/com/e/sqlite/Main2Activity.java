febb59d5fe62aca94ca747c802e3956035999745
Softwarica College Lab <softwarica@softwarica.com>
2019-05-03 14:04:38 +0545

    commit

---
+++ app/src/main/java/com/e/sqlite/Main2Activity.java
@@ -0,0 +1,48 @@
+package com.e.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import helper.MyHelper;
+import model.WordClass;
+
+public class Main2Activity extends AppCompatActivity {
+    private ListView lvwords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        lvwords = findViewById(R.id.lvwords);
+        LoadWord();
+
+    }
+    private void LoadWord() {
+        final MyHelper myHelper = new MyHelper(this);
+        final SQLiteDatabase sqLiteDatabase = myHelper.getWritableDatabase();
+
+        List<WordClass> wordList = new ArrayList<>();
+        wordList = myHelper.GetAllWords(sqLiteDatabase);
+
+        HashMap<String,String> hashMap = new HashMap<>();
+
+        for (int i = 0; i < wordList.size(); i++) {
+            hashMap.put(wordList.get(i).getWord(),wordList.get(i).getMeaning());
+        }
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<> (
+           this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(hashMap.keySet())
+
+        );
+        lvwords.setAdapter(stringArrayAdapter);
+    }
+}
--- app/src/main/java/helper/MyHelper.java
+++ app/src/main/java/helper/MyHelper.java
@@ -2,18 +2,24 @@ package helper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.WordClass;
+
 public class MyHelper extends SQLiteOpenHelper {
 
-    private static  final  String databaseName = "DictionaryDB";
-    private static final int dbVersion =1;
+    private static final String databaseName = "DictionaryDB";
+    private static final int dbVersion = 1;
 
-    private static final  String tblWord="tblWord";
-    private static final String WordID="WordID";
+    private static final String tblWord = "tblWord";
+    private static final String WordID = "WordID";
     private static final String Word = "Word";
-    private static final String Meaning ="Meaning";
+    private static final String Meaning = "Meaning";
 
 
     public MyHelper(Context context) {
@@ -23,7 +29,7 @@ public class MyHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String query ="CREATE TABLE " + tblWord + "(" + WordID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
+        String query = "CREATE TABLE " + tblWord + "(" + WordID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
                 + Word + " TEXT," + Meaning + " TEXT " + ")";
 
         db.execSQL(query);
@@ -42,5 +48,16 @@ public class MyHelper extends SQLiteOpenHelper {
         contentValues.put(Meaning, meaning);
         id = db.insert(tblWord, null, contentValues);
         return id;
+    }
+
+    public List<WordClass> GetAllWords(SQLiteDatabase db) {
+        List<WordClass> dictionaryList = new ArrayList<>();
+        Cursor cursor = db.rawQuery("select * from tblWord", null);
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                dictionaryList.add(new WordClass(cursor.getInt(0), cursor.getString(1), cursor.getString(2)));
+            }
+        }
+        return dictionaryList;
     }
 }
